5d6aa552adb6da81aaf7bb84fead1a3adae575bc
sandeeepsandyy <sandeeps808@gmail.com>
2020-12-20 22:28:15 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component} from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import {CardList} from './components/card_list/card-list.component.jsx';
+
+
+class App extends Component{
+
+  constructor(){
+    super();
+
+    this.state = {
+      monsters:[]
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
+  
+  }
+
+
+
+  render(){
+    return (
+            <div className="App">
+        <CardList name='sandy'> <h1>{this.state.monsters.map(monster=>(<h1 key={monster.id}>{ monster.name}</h1>))}</h1></CardList>
+      </div>
+    );
+
+  }
+}
+
+export default App;
